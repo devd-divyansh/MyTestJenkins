cd9d2ea005ad166185dfcb897289df3ee29f9218
radhaDeveloper <dakhil.2k@gmail.com>
2024-12-23 18:51:00 +0530

    initial commit

---
+++ MyTestJenkins/src/main/java/com/nt/servlet/MessageServlet.java
@@ -0,0 +1,28 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/wish")
+public class MessageServlet extends HttpServlet {
+
+	@Override
+	public  void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                             PrintWriter pw=resp.getWriter();
+		                
+                             resp.setContentType("text/html");
+	                    
+            pw.println("<h1 style='background-color:blue;color:yellow; text-align:center'>Shri Harivansh Divyansh</h1>");
+	           
+            pw.close();
+	}
+
+	
+	
+}
